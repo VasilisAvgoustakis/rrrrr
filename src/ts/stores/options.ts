aa46b73216ad5e687f8461f7fb1691d3f1fd5af5
Christian Stussak <christian.stussak@imaginary.org>
2024-05-08 13:09:29 +0200

    Move option parsing out of option store factory
    This makes it possible to use certain options before vue or pinia are
    initialized.

--- src/ts/stores/options.ts
+++ src/ts/stores/options.ts
@@ -4,34 +4,33 @@ import { defineStore } from 'pinia';
 
 import { NUM_POINTER_MARKERS } from '../builtin-config';
 
+const searchParams = new URLSearchParams(window.location.search);
+
+const useTuioMarkers = (searchParams.get('tuio') ?? 'true') === 'true';
+const usePointerMarkers = (searchParams.get('pointer') ?? 'false') === 'true';
+
+const numPointerMarkers = Number.parseInt(
+  searchParams.get('numPointerMarkers') ?? `${NUM_POINTER_MARKERS}`,
+  10,
+);
+const tuioUrl = searchParams.get('tuioUrl') ?? 'ws://localhost:3339';
+
+const tuioReconnectionDelayMs =
+  Number.parseInt(searchParams.get('tuioReconnectionDelay') ?? '10', 10) * 1000;
+
+const autoStart = (searchParams.get('autoStart') ?? 'true') === 'true';
+
+const developerMode = (searchParams.get('devMode') ?? 'false') === 'true';
+
+const highlightDerivatives =
+  (searchParams.get('highlightDerivatives') ?? 'true') === 'true';
+
+const markerSlotLabels =
+  (searchParams.get('markerSlotLabels') ?? 'true') === 'true';
+
+const autoReset = (searchParams.get('autoReset') ?? 'true') === 'true';
+
 export const useOptionStore = defineStore('options', () => {
-  const searchParams = new URLSearchParams(window.location.search);
-
-  const useTuioMarkers = (searchParams.get('tuio') ?? 'true') === 'true';
-  const usePointerMarkers = (searchParams.get('pointer') ?? 'false') === 'true';
-
-  const numPointerMarkers = Number.parseInt(
-    searchParams.get('numPointerMarkers') ?? `${NUM_POINTER_MARKERS}`,
-    10,
-  );
-  const tuioUrl = searchParams.get('tuioUrl') ?? 'ws://localhost:3339';
-
-  const tuioReconnectionDelayMs =
-    Number.parseInt(searchParams.get('tuioReconnectionDelay') ?? '10', 10) *
-    1000;
-
-  const autoStart = (searchParams.get('autoStart') ?? 'true') === 'true';
-
-  const developerMode = (searchParams.get('devMode') ?? 'false') === 'true';
-
-  const highlightDerivatives =
-    (searchParams.get('highlightDerivatives') ?? 'true') === 'true';
-
-  const markerSlotLabels =
-    (searchParams.get('markerSlotLabels') ?? 'true') === 'true';
-
-  const autoReset = (searchParams.get('autoReset') ?? 'true') === 'true';
-
   const result = {
     useTuioMarkers,
     usePointerMarkers,
